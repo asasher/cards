bb22638398bb143872b6cfa0c59601419e2f3294
Asher <as.asher.anjum@gmail.com>
2026-02-20 20:03:36 +0400

    Add nitro

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   server: {
@@ -17,6 +18,7 @@ const config = defineConfig({
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 });
